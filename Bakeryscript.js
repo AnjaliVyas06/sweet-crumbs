0bf190d254bfb4d1a9c57157e1ddc013ffadbdab
Anjali <vyasanjali2006@gmail.com>
2026-05-30 14:58:59 +0530

    Initial bakery website

---
+++ Bakeryscript.js
@@ -0,0 +1,27 @@
+// let cupckaePrice = 299;
+// let flavor = "Chocalate Dream";
+// let isAvailable = true;
+
+// console.log(flavor);
+
+// if(cupckaePrice <= 199){
+//     console.log("it isnt that costly");
+// }else{
+//     console.log("it is pretty choice to eat expansive sweets");
+// }
+
+// for(let i =0;i<=5;i++){
+//     console.log("offer!!!");
+// }
+
+let bakeryName = "Sweet Crumbs";
+
+let items = [
+    "Chocalate Dream",
+    "pink velvet",
+    "Strawberry Bliss"
+];
+
+for(let i=0;i<items.length;i++){
+console.log(items[i]);
+}
